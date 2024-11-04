e1427b8e68751555c06f48b88ac1907554285244
MateuszWiktorowicz <83886002+MateuszWiktorowicz@users.noreply.github.com>
2024-11-04 10:10:03 +0100

    tool geometry entity

--- src/Entity/ToolType.php
+++ src/Entity/ToolType.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ToolTypeRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ToolTypeRepository::class)]
@@ -15,6 +17,17 @@ class ToolType
 
     #[ORM\Column(length: 50)]
     private ?string $name = null;
+
+    /**
+     * @var Collection<int, ToolGeometry>
+     */
+    #[ORM\OneToMany(targetEntity: ToolGeometry::class, mappedBy: 'toolType')]
+    private Collection $toolGeometries;
+
+    public function __construct()
+    {
+        $this->toolGeometries = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -29,6 +42,36 @@ class ToolType
     public function setName(string $name): static
     {
         $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ToolGeometry>
+     */
+    public function getToolGeometries(): Collection
+    {
+        return $this->toolGeometries;
+    }
+
+    public function addToolGeometry(ToolGeometry $toolGeometry): static
+    {
+        if (!$this->toolGeometries->contains($toolGeometry)) {
+            $this->toolGeometries->add($toolGeometry);
+            $toolGeometry->setToolType($this);
+        }
+
+        return $this;
+    }
+
+    public function removeToolGeometry(ToolGeometry $toolGeometry): static
+    {
+        if ($this->toolGeometries->removeElement($toolGeometry)) {
+            // set the owning side to null (unless already changed)
+            if ($toolGeometry->getToolType() === $this) {
+                $toolGeometry->setToolType(null);
+            }
+        }
 
         return $this;
     }
